a2ad15ece9d385eb55bd4f552e0416b594d046a2
Byron Samaripa <bsamaripa@lifetimefitness.com>
2015-06-12 13:33:05 -0600

    Begin problem 8

---
+++ src/main/scala/com/byron/problem08.scala
@@ -0,0 +1,22 @@
+package com.byron
+
+import scala.math._
+import scala.io.StdIn
+
+/*
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+ * that the 6th prime is 13.
+ *
+ * What is the 10 001st prime number?
+ *
+ * Resource: http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
+ */
+
+class productSeries{
+
+}
+
+object productSeriesMain {
+  def main(args: Array[String]) {
+  }
+}
